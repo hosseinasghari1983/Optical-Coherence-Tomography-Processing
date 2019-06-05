1da601de8e6ef289124151cbe1cf9c2fec35a06c
Matthew <mlow2@lion.lmu.edu>
2019-06-05 12:07:06 -0700

    Begin experimental branch, rewrite collect code

--- py/Collect.py
+++ py/Collect.py
@@ -6,8 +6,11 @@ import struct
 
 class Collection:
     def __init__(self):
+        scales = [.02, 1, 1, 1]
+        y_pos = [0, 0, 0, 0]
+        sample_rate = 20e9
         self.config = {'ip': '192.168.1.25', 'channels': [1, 0, 0, 0],
-                       'settings': [[.02, 0, 0, 0], [0, 0, 0, 0], 20e9, 0], 'recordLength': 3e4}
+                       'settings': [scales, y_pos, sample_rate, 0], 'recordLength': 3e4}
         rm = visa.ResourceManager()
         for n in range(3):
             try:
@@ -25,20 +28,21 @@ class Collection:
         for ch in [1, 2, 3, 4]:
             active_ch = self.config["channels"][ch - 1]
             if active_ch:
-                self.scope.write('SELECT:CH' + str(ch) + ' on')
-                self.scope.write('CH' + str(ch) + ':SCALE ' + str(self.config['settings'][0][ch - 1]))
+                self.scope.write('SELect:CH' + str(ch) + ' ON')
+                self.scope.write('CH' + str(ch) + ':SCAle ' + str(self.config['settings'][0][ch - 1]))
                 self.scope.write('CH' + str(ch) + ':POSITION ' + str(self.config['settings'][1][ch - 1]))
             else:
-                self.scope.write('SELECT:CH' + str(ch) + ' off')
+                self.scope.write('SELECT:CH' + str(ch) + ' OFF')
 
         sample_rate = self.config['settings'][2]
         hor_pos = self.config['settings'][2]
         record_length = self.config['recordLength']
 
-        self.scope.write('DATA:WIDTH 1')
-        self.scope.write('DATA:ENC SFP')
-        self.scope.write('HORIZONTAL:MAIN:SAMPLERate ' + str(sample_rate))
-        self.scope.write('HORIZONTAL:RECORDLENGTH ' + str(record_length))
+        self.scope.write('DATa:WIDTH 1')
+        self.scope.write('DATa:ENCdg FAStest')
+        self.scope.write('DISplay:WAVEform OFF')
+        self.scope.write('HORizontal:MODE:SAMPLERate ' + str(sample_rate))  # Sets sample rate
+        self.scope.write('HORizontal:MODE:RECOrdlength ' + str(record_length))  # Number of samples
         record_length = self.scope.query("HORIZONTAL:RECORDLENGTH?")
         self.scope.write("data:start 1;stop " + str(record_length) + ";:data:encdg rpbinary;:DESE 1;:*ESE 1")
         self.scope.write('HORizontal:POSition ' + str(hor_pos))
@@ -46,23 +50,25 @@ class Collection:
     def read_data(self):
         if self.failure:
             return -1
-        yMult = float(self.scope.query('WFMPRE:YMULT?'))
-        yZero = float(self.scope.query('WFMPRE:YZERO?'))
-        yOff = float(self.scope.query('WFMPRE:YOFF?'))
+
+        y_mult = float(self.scope.query('WFMPRE:YMULT?'))
+        y_zero = float(self.scope.query('WFMPRE:YZERO?'))
+        y_off = float(self.scope.query('WFMPRE:YOFF?'))
         x_incr = float(self.scope.query('WFMPRE:XINCR?'))
 
         #     single shot
-        self.scope.write('ACQUIRE:STATE STOP')
-        self.scope.write('ACQUIRE:MODE SAMPLE')
-        self.scope.write('ACQUIRE:SAMPLINGMODE RT')
-        self.scope.write('ACQUIRE:STOPAFTER SEQuence')
+        # self.scope.write('ACQUIRE:STATE STOP')
+        # self.scope.write('ACQUIRE:MODE SAMPLE')
+        # self.scope.write('ACQUIRE:SAMPLINGMODE RT')
+        # self.scope.write('ACQUIRE:STOPAFTER SEQuence')
 
-        self.scope.write('HORIZONTAL:FASTFRAME:STATE 1')
+        self.scope.write('HORizontal:FASTframe:STATE ON')
+        self.scope.write('HORizontal: FASTframe:SELECTED: SOUrce CH' + str(self.config["channels"][0]))
         self.scope.write('ACQUIRE:STATE RUN')
 
         # print('Rel WL process.')
 
-        self.scope.write('DATA:SOU CH' + str(self.config["channels"][0]))
+        self.scope.write('DATa:SOUrce CH' + str(self.config["channels"][0]))
         self.scope.write('CURVE?')
         osc_data = self.scope.read_raw()
         header_len = 2 + int(osc_data[1])
@@ -71,7 +77,7 @@ class Collection:
 
         # print(f'ADC_wave is {ADC_wave} with shape {ADC_wave.shape}')
 
-        sig = (waveform - yOff) * yMult + yZero
+        sig = (waveform - y_off) * y_mult + y_zero
         time = np.linspace(0, x_incr * len(sig) * 1e9, len(sig))
         self.osc_sig = sig
         self.osc_time = time
