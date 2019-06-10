cdbfb1fa0dd359062141ec13a5f8ced4353440a5
pedro-sales <pedrosalesrodriguez@gmail.com>
2019-06-10 15:17:25 -0700

    Faster update rate

--- py/Main.py
+++ py/Main.py
@@ -6,7 +6,8 @@ import visualize as vis
 scales = [.02, 1, 1, 1]  # set on scope
 y_pos = [0, 0, 0, 0]  # set on scope
 sample_rate = 12.5e9
-config = {'ip': '10.5.97.239', 'channels': [1, 0, 0, 0], 'settings': [scales, y_pos, sample_rate, 0],
+#'10.5.97.239'
+config = {'ip': '192.168.0.2', 'channels': [1, 0, 0, 0], 'settings': [scales, y_pos, sample_rate, 0],
           'record_length': 1000, 'samp_clk_ch': 2, 'data_ch': 1, 'laser_ref_ch': 3, 'frame_count': 2500, 'interp_factor':1}
 
 raw_queue = queue.LifoQueue()
